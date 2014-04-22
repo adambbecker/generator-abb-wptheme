07b97b68d777e670c47fd6e45b2369786b510336
Adam Becker <adambbecker@gmail.com>
2014-04-21 21:23:05 -0400

    Added php comments to header

--- app/templates/theme_files/header.php
+++ app/templates/theme_files/header.php
@@ -1,6 +1,15 @@
-<!DOCTYPE html>
-<!--[if lte IE 9]>      <html <?php language_attributes(); ?> class="no-js ie lt-ie10"> <![endif]-->
-<!--[if gt IE 9]><!--> <html <?php language_attributes(); ?> class="no-js"> <!--<![endif]-->
+<?php
+/**
+ * The Header for our theme.
+ *
+ * Displays everything up to opening body tag
+ *
+ * @package WordPress
+ * @subpackage <%= themeTitle %> theme
+ * @since 1.0.0
+ */
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js">
 
 <head>
 
@@ -8,16 +17,16 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
 	<?php if (is_search()) : ?>
-	<meta name="robots" content="noindex, nofollow" /> 
+	<meta name="robots" content="noindex, nofollow" />
 	<?php endif; ?>
-	
+
 	<title><?php wp_title(' | ', true, 'right'); ?><?php bloginfo('name'); ?></title>
 	<meta name="description" content="<?php bloginfo( 'description' ); ?>">
-	
+
 	<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>">
 	<link rel="alternate" type="application/rss+xml" title="<?php bloginfo( 'name' ); ?> RSS Feed" href="<?php bloginfo( 'rss2_url' ); ?>" />
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-	
+
 	<?php wp_head(); ?>
 
 </head>
